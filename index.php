d3c58ddf9ad5ea166a3d54290901ff48a43fee1d
p33rs <jon@peers.pw>
2014-09-22 04:30:53 -0400

    good enough

--- index.php
+++ index.php
@@ -10,35 +10,42 @@
   <link href='http://fonts.googleapis.com/css?family=Rubik+One' rel='stylesheet' type='text/css'>
   
   <script src="https://code.jquery.com/jquery-1.11.1.min.js" type="text/javascript"></script>
+  
   <script type="text/javascript">
-    var randInt = function(min, max) {
-        if (typeof min === 'undefined') min = 0;
-        if (typeof max === 'undefined') max = 100;
-        Math.floor(Math.random() * (max - min)) + min;
-    }
-    var catstorm = function() {
-        var cat = $('<img />')
-            .attr('src', 'nyancat.gif')
-            .css({
-                position: 'fixed',
-                left: window.innerWidth,
-                top:  randInt(0, window.innerHeight)
-            })
-            .animate(
-                { left: 0 },
-                randInt(100,400)
-            );
-    };
-    $(function() {  
+    $(function() {
+        var rand = function(min, max) {
+            if (typeof min === 'undefined') min = 0;
+            if (typeof max === 'undefined') max = 1;
+            if (min > max) {
+                throw new RangeError('requested invalid range');
+            }
+            return Math.floor(Math.random() * (max - min)) + min;
+        };
+        var words = [
+            'has a github',
+            'has a github',
+            'has a github',
+            'is rad',
+            'never used venmo',
+            'develops internets',
+            'rocks out',
+            'designs apis',
+            'develops javascript',
+            'obsesses',
+            'wrote this at 4 am',
+            'is not a ninja',
+            'needs a job',
+            'つ ◕_◕ ༽つ'
+        ];
         $('#man_why_you_even_got_to_do_a_thing').on('mouseover', function() {
             $('#fuck_yes_i_am_awesome').hide();
-            $('#anticipa___________________tion').show();
+            $('#anticipa___________________tion').text(words[rand(0, words.length - 1)]).show();
         }).on('mouseleave', function() {
             $('#anticipa___________________tion').hide();
             $('#fuck_yes_i_am_awesome').show();
-        }).on('click', function() {
-            $('#man_why_you_even_got_to_do_a_thing').hide();
-            $('#the_metals_kiss_and_the_fuel_turn_lively').show();
+        }).on('click', function(e) {
+            e.preventDefault();
+            window.location = 'https://github.com/p33rs';
         });
     });
   </script>
@@ -75,11 +82,6 @@
     <section id="man_why_you_even_got_to_do_a_thing">
         <p id="fuck_yes_i_am_awesome">jon pierce</p>
         <p id="anticipa___________________tion">(click)</p>
-    </section>
-    <section id="the_metals_kiss_and_the_fuel_turn_lively">
-        <a id="jfc_have_you_seen_this_fucking_repo">
-            <img id="it_is_a_perfect___logo" src="github.png" alt="Github" title="I'd fork that." />
-        </a>
     </section>
 </body>
 </html>
